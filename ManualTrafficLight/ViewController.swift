2e5f2aa06fa23b8f170d4c9e410209bf5c77e084
Embler <vbembler@gmail.com>
2020-08-19 22:40:45 +0300

    Created interface and logic for HW 2

--- ManualTrafficLight/ViewController.swift
+++ ManualTrafficLight/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var toggleButton: UIButton!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        toggleButton.layer.cornerRadius = 10
+        redView.layer.cornerRadius = 30
+        yellowView.layer.cornerRadius = 30
+        greenView.layer.cornerRadius = 30
     }
 
-
+    @IBAction func toggleButtonPressed() {
+        let valueOfEnabled = CGFloat(1)
+        
+        switch valueOfEnabled {
+        case redView.alpha:
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+        case yellowView.alpha:
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+        case greenView.alpha:
+            greenView.alpha = 0.3
+            redView.alpha = 1
+        default:
+            redView.alpha = 1
+            toggleButton.setTitle("Next", for: .normal)
+        }
+    }
 }
 
